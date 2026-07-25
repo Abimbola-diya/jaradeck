21dca18bb9d6821cfaf7439e94bc0e62ba5903b4
abimbola <abimbolaogundiya2@gmail.com>
2026-07-25 15:31:35 +0100

    wavy canopy

--- frontend/src/components/StadiumIllustration.jsx
+++ frontend/src/components/StadiumIllustration.jsx
@@ -4,6 +4,9 @@ export default function StadiumIllustration() {
   return (
     <div className="stadium-wrapper">
       <img src="/hero_graphics.svg" alt="Jaradeck Hero Illustration" className="stadium-svg" />
+      <div className="stadium-canopy-container">
+        <img src="/canopy.svg" alt="White Canopy Tip" className="stadium-canopy-img" />
+      </div>
     </div>
   );
 }
