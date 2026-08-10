896319af5d6e60c578cb35587f8d05b5620ade3d
abimbola <abimbolaogundiya2@gmail.com>
2026-08-10 13:11:27 +0100

    cleaned up requirements.txt

--- backend/test_pg.js
+++
@@ -1,8 +0,0 @@
-const { Client } = require('pg');
-const client = new Client({
-  connectionString: 'postgresql://postgres:abimbola@2007db@db.yjzfotmjkziehsqvxito.supabase.co:5432/postgres'
-});
-client.connect()
-  .then(() => console.log('Connected!'))
-  .catch(e => console.error(e.message))
-  .finally(() => client.end());
